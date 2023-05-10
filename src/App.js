1eabec4c32707169bf969da17ba862b9b85cccb2
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-05-10 17:28:34 -0400

    lab done to iteration 7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foods from './foods.json';
+import { useState } from 'react';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
+  const [food, setFood] = useState(foods);
+  const [foodData, setFoodData] = useState(foods);
+  const [search, setSearch] = useState("");
+  const [showAdd, setShowAdd] = useState(true);
+
+  const addNewFood = (newFood) => {
+    const updatedFood = [newFood, ...food];
+    setFood(updatedFood);
+    setFoodData(updatedFood);
+  };
+
+  const removeFood = (foodName) => {
+    setFood(
+      food.filter((f) => {
+        return f.name !== foodName;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+      <Search setSearch={setSearch} search={search}/>
+
+      <button onClick={() => setShowAdd(!showAdd)}>{showAdd ? "Hide" : "Show"}</button>
+      {showAdd && <AddFoodForm addFood={addNewFood} />}
+      <div>
+        <Row>
+          {food.filter((curr) => curr.name.toLowerCase().includes(search.toLowerCase())).map((f) => {
+            return (
+              <FoodBox key={f._id} food={f} foods={food} setFood={setFood} removeFood={removeFood}/>
+            );
+          })}
+        </Row>
+      </div>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+
+const Search = ({ setSearch, search }) => {
+
+  return (
+    <div>
+      <label htmlFor="search">Search</label>
+      <Input
+        type="text"
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      ></Input>
+    </div>
+  );
+};
+
+export default Search;
